404dfc03dbcf689bc5f8766cb24df2a0f4c2dd6d
didiermis <didier.mis.may@gmail.com>
2023-03-14 16:54:27 -0600

    removed the overwrite entry for the Alice seed

--- pallets/nicks/src/execute_tx.sh
+++ pallets/nicks/src/execute_tx.sh
@@ -7,5 +7,3 @@ polkadot-js-api tx.nicks.setName "AlexaXRP1" "SolarisRP1" --seed "//Alice//stash
 polkadot-js-api tx.nicks.setName "MarianaXRP1" "SolanaXRP1" --seed "//Bob"
 
 polkadot-js-api tx.nicks.setName "CarlosXRP1" "CalciumXRP1" --seed "//Bob//stash"
-
-polkadot-js-api tx.nicks.setName "AlejandroXRP1" "AnquinouesXRP1" --seed "//Alice"
