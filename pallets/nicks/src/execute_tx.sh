c0d2d5a21a368d705e555404581767354eec4c46
didiermis <didier.mis.may@gmail.com>
2023-03-14 16:48:30 -0600

    Created a script to populate the nicks pallet

---
+++ pallets/nicks/src/execute_tx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+polkadot-js-api tx.nicks.setName "DanteXRP1" "MaximunXRP1" --seed "//Alice"
+
+polkadot-js-api tx.nicks.setName "AlexaXRP1" "SolarisRP1" --seed "//Alice//stash"
+
+polkadot-js-api tx.nicks.setName "MarianaXRP1" "SolanaXRP1" --seed "//Bob"
+
+polkadot-js-api tx.nicks.setName "CarlosXRP1" "CalciumXRP1" --seed "//Bob//stash"
+
+polkadot-js-api tx.nicks.setName "AlejandroXRP1" "AnquinouesXRP1" --seed "//Alice"
