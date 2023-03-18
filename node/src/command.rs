42e1071cc1e0321b3d8182d027bb2fe7cc09eda4
didiermis <didier.mis.may@gmail.com>
2023-03-18 09:24:12 -0600

    Fixed the TryRuntime subcommand. Fixed the try-runtime-cli implementation.

--- node/src/command.rs
+++ node/src/command.rs
@@ -173,42 +173,31 @@ pub fn run() -> sc_cli::Result<()> {
 			})
 		},
 		#[cfg(feature = "try-runtime")]
+		#[cfg(feature = "try-runtime")]
 		Some(Subcommand::TryRuntime(cmd)) => {
+			use crate::service::ExecutorDispatch;
+			use sc_executor::{sp_wasm_interface::ExtendedHostFunctions, NativeExecutionDispatch};
 			let runner = cli.create_runner(cmd)?;
 			runner.async_run(|config| {
-				// only need a runtime or a task manager to do `async_run`.
+				// we don't need any of the components of new_partial, just a runtime, or a task
+				// manager to do `async_run`.
 				let registry = config.prometheus_config.as_ref().map(|cfg| &cfg.registry);
-				let task_manager = sc_service::TaskManager::new(
-				config.tokio_handle.clone(),
-					registry,
-				).map_err(|e| sc_cli::Error::Service(sc_service::Error::Prometheus(e)))?;
-				
-			Ok((cmd.run::<Block, service::ExecutorDispatch>(config), task_manager))
+				let task_manager =
+					sc_service::TaskManager::new(config.tokio_handle.clone(), registry)
+						.map_err(|e| sc_cli::Error::Service(sc_service::Error::Prometheus(e)))?;
+				Ok((
+					cmd.run::<Block, ExtendedHostFunctions<
+						sp_io::SubstrateHostFunctions,
+						<ExecutorDispatch as NativeExecutionDispatch>::ExtendHostFunctions,
+					>>(),
+					task_manager,
+				))
 			})
 		},
-
 		#[cfg(not(feature = "try-runtime"))]
-		Some(Subcommand::TryRuntime) => {
-			Err("TryRuntime wasn't enabled when building the node. \
-			You can enable it with `--features try-runtime`.".into())
-			},
-	// #[cfg(feature = "try-runtime")]
-	// Some(Subcommand::TryRuntime(cmd)) => {
-	// 	let runner = cli.create_runner(cmd)?;
-	// 	runner.async_run(|config| {
-	// 		// we don't need any of the components of new_partial, just a runtime, or a task
-	// 		// manager to do `async_run`.
-	// 		let registry = config.prometheus_config.as_ref().map(|cfg| &cfg.registry);
-	// 		let task_manager =
-	// 			sc_service::TaskManager::new(config.tokio_handle.clone(), registry)
-	// 				.map_err(|e| sc_cli::Error::Service(sc_service::Error::Prometheus(e)))?;
-	// 		Ok((cmd.run::<Block, service::ExecutorDispatch>(config), task_manager))
-	// 	})
-	// },
-	// #[cfg(not(feature = "try-runtime"))]
-	// Some(Subcommand::TryRuntime) => Err("TryRuntime wasn't enabled when building the node. \
-	// 		You can enable it with `--features try-runtime`."
-	// 	.into()),
+		Some(Subcommand::TryRuntime) => Err("TryRuntime wasn't enabled when building the node. \
+				You can enable it with `--features try-runtime`."
+			.into()),
 		Some(Subcommand::ChainInfo(cmd)) => {
 			let runner = cli.create_runner(cmd)?;
 			runner.sync_run(|config| cmd.run::<Block>(&config))
