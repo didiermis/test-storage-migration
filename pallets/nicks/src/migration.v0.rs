e5a6bcff1c81e54efa87c1dc42a3b96e2994a531
didiermis <didier.mis.may@gmail.com>
2023-03-22 11:51:19 -0600

    Failed adaption of the migration v0 using the frame-executive module

--- pallets/nicks/src/migration.v0.rs
+++ pallets/nicks/src/migration.v0.rs
@@ -19,7 +19,7 @@
 use super::*;
 use log::info;
 use frame_support::{
-	traits::{Get, StorageVersion, GetStorageVersion},
+	traits::{Get, StorageVersion, GetStorageVersion, OnRuntimeUpgrade},
 	weights::Weight, storage_alias, Twox64Concat, BoundedVec, log
 };
 const LOG_TARGET: &str = "nicks";
@@ -28,58 +28,129 @@ const LOG_TARGET: &str = "nicks";
 // only contains V1 storage format
 pub mod v1 {
     use super::*;
+	// use crate::Config;
+	use frame_support::{pallet_prelude::*, weights::Weight};
+	use sp_runtime::Saturating;
+
     type BalanceOf<T> = <<T as Config>::Currency as Currency<AccountIdOf<T>>>::Balance;
 
-    #[storage_alias]
-	pub(super) type NameOf<T: Config> =
-		StorageMap<Pallet<T>, Twox64Concat, <T as frame_system::Config>::AccountId, (BoundedVec<u8, <T as pallet::Config>::MaxLength>, BalanceOf<T>)>;
-} 
+    // #[storage_alias]
+	// pub(super) type NameOf<T: Config> =
+	// 	StorageMap<Pallet<T>, Twox64Concat, <T as frame_system::Config>::AccountId, (BoundedVec<u8, <T as pallet::Config>::MaxLength>, BalanceOf<T>)>;
+	
+	#[derive(Encode, Decode)]
+	pub struct NickName<T: Config> {
+		pub first: BoundedVec<u8, T::MaxLength>,
+		pub last: Option<BoundedVec<u8, T::MaxLength>>,
+	}
 
+	pub struct MigrateToV1<T>(sp_std::marker::PhantomData<T>);
+	impl <T: Config> OnRuntimeUpgrade for MigrateToV1<T> {
+		fn on_runtime_upgrade() -> Weight {
+			let onchain_version =  Pallet::<T>::on_chain_storage_version();
+			let current_version = Pallet::<T>::current_storage_version();
 
-// contains checks and transforms storage to V2 format
-pub fn migrate_to_v2<T: Config>() -> Weight {
-    let onchain_version =  Pallet::<T>::on_chain_storage_version();
-    if onchain_version < 2 {
-            // migrate to v2
-            // Very inefficient, mostly here for illustration purposes.
-			let count = v1::NameOf::<T>::iter().count();
-			info!(target: LOG_TARGET, " >>> Updating MyNicks storage. Migrating {} nicknames...", count);
-
-			// We transform the storage values from the old into the new format.
-			NameOf::<T>::translate::<(Vec<u8>, BalanceOf<T>), _>(
-				|k: T::AccountId, (nick, deposit): (Vec<u8>, BalanceOf<T>)| {
-					info!(target: LOG_TARGET, "     Migrated nickname for {:?}...", k);
-
-					// We split the nick at ' ' (<space>).
-					match nick.iter().rposition(|&x| x == b" "[0]) {
-						Some(ndx) => {
-                            let bounded_first: BoundedVec<_, _> = nick[0..ndx].to_vec().try_into().unwrap();
-                            let bounded_last: BoundedVec<_, _> = nick[ndx + 1..].to_vec().try_into().unwrap();
-                            Some((Nickname {
-							    first: bounded_first,
-							    last: Some(bounded_last)
-						    }, deposit))
-                    },
-						None => {
-                            let bounded_name: BoundedVec<_, _> = nick.to_vec().try_into().unwrap();
-                            Some((Nickname { first: bounded_name, last: None }, deposit))
-                        }
-					}
-				}
+			let count = NameOf::<T>::iter().count();
+			let mut translated = 0u64;
+	
+			log::info!(
+				target: LOG_TARGET,
+				"Running migration with current storage version {:?} / onchain {:?}",
+				current_version,
+				onchain_version
 			);
 
-			// Update storage version.
-			StorageVersion::new(2).put::<Pallet::<T>>();
-			// Very inefficient, mostly here for illustration purposes.
-			let count = NameOf::<T>::iter().count();
-			info!(target: LOG_TARGET," <<< MyNicks storage updated! Migrated {} nicknames ✅", count);
-			// Return the weight consumed by the migration.
-			T::DbWeight::get().reads_writes(count as u64 + 1, count as u64 + 1)
+			if onchain_version == 0 && current_version == 1 {
 
-    } else {
-        info!(target: LOG_TARGET, " >>> Unused migration!");
-        // We don't do anything here.
-		Weight::zero()
-    }
+				// We transform the storage values from the old into the new format.
+				// NameOf::<T>::translate::<(Vec<u8>, BalanceOf<T>), _>(
+				NameOf::<T>::translate(
+					|key: T::AccountId, (nick, deposit): (Vec<u8>, BalanceOf<T>)| {
+						info!(target: LOG_TARGET, "     Migrated nickname for {:?}...", key);
+						translated.saturating_inc();
 
-} 
+						// We split the nick at ' ' (<space>).
+						match nick.iter().rposition(|&x| x == b" "[0]) {
+							Some(ndx) => {
+								let bounded_first: BoundedVec<_, _> = nick[0..ndx].to_vec().try_into().unwrap();
+								let bounded_last: BoundedVec<_, _> = nick[ndx + 1..].to_vec().try_into().unwrap();
+								Some((Nickname {
+									first: bounded_first,
+									last: Some(bounded_last)
+								}, deposit))
+						},
+							None => {
+								let bounded_name: BoundedVec<_, _> = nick.to_vec().try_into().unwrap();
+								Some((Nickname { first: bounded_name, last: None }, deposit))
+							}
+						}
+					}
+				);
+
+
+				current_version.put::<Pallet<T>>();
+
+				log::info!(
+					target: LOG_TARGET,
+					"Upgraded {} names from {} initial names, storage to version {:?}",
+					count,
+					translated,
+					current_version
+				);
+
+				T::DbWeight::get().reads_writes(translated + 1, translated + 1)
+			} else {
+				log::info!(
+					target: LOG_TARGET,
+					"Migration did not execute. This probably should be removed"
+				);
+				T::DbWeight::get().reads(1)
+			}
+		}
+
+		#[cfg(feature = "try-runtime")]
+		fn pre_upgrade() -> Result<Vec<u8>, &'static str> {
+			let current_version = Pallet::<T>::current_storage_version();
+			let onchain_version = Pallet::<T>::on_chain_storage_version();
+			ensure!(
+				onchain_version == 0,
+				"must upgrade linearly"
+			);
+			ensure!(
+				current_version == 1,
+				"migration from version 0 to 1."
+			);
+			let prev_count = NameOf::<T>::iter().count();
+			Ok((prev_count as u32).encode())
+		}
+	
+		#[cfg(feature = "try-runtime")]
+		fn post_upgrade(prev_count: Vec<u8>) -> Result<(), &'static str> {
+			let prev_count: u32 = Decode::decode(&mut prev_count.as_slice()).expect(
+				"the state parameter should be something that was generated by pre_upgrade",
+			);
+			let post_count = NameOf::<T>::iter().count() as u32;
+			assert_eq!(
+				prev_count, post_count,
+				"the records count before and after the migration should be the same"
+			);
+	
+			let current_version = Pallet::<T>::current_storage_version();
+			let onchain_version = Pallet::<T>::on_chain_storage_version();
+	
+			ensure!(current_version == 1, "must_upgrade to v1");
+			assert_eq!(
+				current_version, onchain_version,
+				"after migration, the current_version and onchain_version should be the same"
+			);
+			NameOf::<T>::iter().for_each(|(key, (nicks, deposit))| {
+				assert!(nicks.last.is_none(),
+				"accounts should only be Active. None should be in Inactive status, or undefined state")
+			});
+			Ok(())
+		}
+		
+	}
+}
+
+
